40f93fad419b6e29c7ea3ebba216e5b77ceb4b6b
Anand <kikyak2020@gmail.com>
2020-10-09 13:24:12 +0500

    first commit

--- route/jobticket.js
+++ route/jobticket.js
@@ -14,21 +14,20 @@ const s3 = new aws.S3({
     Bucket: 'werkzbucket'
 });
 
-
 const profileImgUpload = multer({
     storage: multerS3({
-        s3: s3,
-        bucket: 'werkzbucket',
-        acl: 'public-read',
-        key: function (req, file, cb) {
-            cb(null, '');
-        },
+     s3: s3,
+     bucket: 'werkzbucket',
+     acl: 'public-read',
+     key: function (req, file, cb) {
+      cb(null, path.basename( file.originalname, path.extname( file.originalname ) ) + '-' + Date.now() + path.extname( file.originalname ) )
+     }
     }),
-    limits: { fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
+    limits:{ fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
+    fileFilter: function( req, file, cb ){
+     checkFileType( file, cb );
     }
-}).single('file');
+   }).single('profileImage');
 
 function checkFileType(file, cb) {
     // Allowed ext
@@ -43,9 +42,34 @@ function checkFileType(file, cb) {
         cb('Error: Images Only!');
     }
 }
+router.post( '/profile-img-upload', ( req, res ) => {
+    profileImgUpload( req, res, ( error ) => {
+      // console.log( 'requestOkokok', req.file );
+      // console.log( 'error', error );
+      if( error ){
+       console.log( 'errors', error );
+       res.json( { error: error } );
+      } else {
+       // If File not found
+       if( req.file === undefined ){
+        console.log( 'Error: No File Selected!' );
+        res.json( 'Error: No File Selected' );
+       } else {
+        // If Success
+        const imageName = req.file.key;
+        const imageLocation = req.file.location;
+    // Save the file name into database into profile model
+    res.json( {
+         image: imageName,
+         location: imageLocation
+        } );
+       }
+      }
+     });
+    });
 router.post('/addjobTicketImage', (req, res) => {
-    console.log(req);
-    console.log(res);
+    console.log(req.image);
+    console.log(req.file);
     profileImgUpload(req, res, (error) => {
         // console.log( 'requestOkokok', req.file );
         // console.log( 'error', error );
